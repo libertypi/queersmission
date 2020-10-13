f3f1724da4d09f02385b55f354fef5e84075ee85
libertypi <libertypi@gmail.com>
2020-10-14 00:57:20 +0800

    .

--- component/rebuilder.py
+++ component/rebuilder.py
@@ -3,25 +3,22 @@
 import os.path
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-import regenerator as regen
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../regenerator")))
+from regenerator import Regen
 
 
-def read_file(file: str, extractedWordlist: bool = False):
+def read_file(file: str, extractWriteback: bool = False) -> Regen:
 
     path = os.path.join(os.path.dirname(__file__), file)
 
     with open(path, mode="r+", encoding="utf-8") as f:
 
         o_list = f.read().splitlines()
-        s_list = {i.lower() for i in o_list if i}
-        extracters = tuple(map(regen.Extractor, s_list))
+        s_list = sorted({i.lower() for i in o_list if i})
+        regen = Regen(s_list)
 
-        if extractedWordlist:
-            s_list = [j for i in extracters for j in i.get_text()]
-        else:
-            s_list = list(s_list)
-        s_list.sort()
+        if extractWriteback:
+            s_list = regen.to_text()
 
         if o_list != s_list:
             f.seek(0)
@@ -30,7 +27,26 @@ def read_file(file: str, extractedWordlist: bool = False):
             f.truncate()
             print(f"{file} updated.")
 
-    return extracters, s_list
+    return regen
+
+
+def optimize_regex(regen: Regen, name: str) -> str:
+
+    wordlist = regen.wordlist
+    computed = regen.to_regex()
+
+    concat = "|".join(wordlist)
+    if len(wordlist) > 1:
+        concat = f"({concat})"
+
+    diff = len(computed) - len(concat)
+    if diff > 0:
+        print(f"{name}: Computed regex is {diff} characters longer than concatenation, use the latter.")
+        return concat
+
+    regen.verify_result()
+    print(f"{name}: Regex test passed. Characters saved: {-diff}.")
+    return computed
 
 
 def write_file(file: str, content: str, checkDiff: bool = True):
@@ -51,52 +67,37 @@ def write_file(file: str, content: str, checkDiff: bool = True):
         print(f"{file} updated.")
 
 
-def optimize_regex(extractors: list, wordlist: list, name: str, test: bool = True) -> str:
-    computed = regen.Optimizer(*extractors).result
-
-    concat = "|".join(wordlist)
-    if len(wordlist) > 1:
-        concat = f"({concat})"
-
-    diff = len(computed) - len(concat)
-    if diff > 0:
-        print(f"{name}: Computed regex is {diff} characters longer than concatenation, use the latter.")
-        return concat
-
-    if test:
-        regen.test_regex(regex=computed, wordlist=wordlist)
-        print(f"{name}: Regex test passed. Characters saved: {-diff}.")
-
-    return computed
+def remove_outer_parentheses(s: str):
+    if s.startswith("(") and s.endswith(")") and s.count("(", 1, -1) == s.count(")", 1, -1):
+        return s[1:-1]
+    return s
 
 
 def main():
 
-    kwExtractors, kwList = read_file("av_keyword.txt", extractedWordlist=False)
-    cidExtractors, cidList = read_file("av_censored_id.txt", extractedWordlist=True)
-    ucidExtractors, ucidList = read_file("av_uncensored_id.txt", extractedWordlist=True)
+    kwRegen = read_file("av_keyword.txt", extractWriteback=False)
+    cidRegen = read_file("av_censored_id.txt", extractWriteback=True)
+    ucidRegen = read_file("av_uncensored_id.txt", extractWriteback=True)
 
-    remove = set(ucidList)
-    removeLength = len(remove)
-    remove.difference_update(cidList)
-    if len(remove) != removeLength:
-        ucidList = sorted(remove)
-        ucidExtractors = map(regen.Extractor, remove)
-        write_file("av_uncensored_id.txt", "\n".join(ucidList) + "\n", checkDiff=False)
+    source = set(ucidRegen.to_text())
+    sourceLen = len(source)
+    source.difference_update(cidRegen.to_text())
+    if sourceLen != len(source):
+        source = sorted(source)
+        ucidRegen = Regen(source)
+        write_file("av_uncensored_id.txt", "\n".join(source) + "\n", checkDiff=False)
 
-    av_keyword = optimize_regex(kwExtractors, kwList, "Keywords", False)
-    if av_keyword.startswith("(") and av_keyword.endswith(")"):
-        av_keyword = av_keyword[1:-1]
+    av_keyword = optimize_regex(kwRegen, "Keywords")
+    av_keyword = remove_outer_parentheses(av_keyword)
+    av_censored_id = optimize_regex(cidRegen, "Censored ID")
+    av_uncensored_id = optimize_regex(ucidRegen, "Uncensored ID")
 
-    av_censored_id = optimize_regex(cidExtractors, cidList, "Censored ID")
-    av_uncensored_id = optimize_regex(ucidExtractors, ucidList, "Uncensored ID")
-
-    avReg = f"(^|[^a-z0-9])({av_keyword}|{av_uncensored_id}[ _-]*[0-9]{{2,6}}|[0-9]{{,4}}{av_censored_id}[ _-]*[0-9]{{2,6}})([^a-z0-9]|$)\n"
-    write_file("av_regex.txt", avReg, checkDiff=True)
+    avRegex = f"(^|[^a-z0-9])({av_keyword}|{av_uncensored_id}[ _-]*[0-9]{{2,6}}|[0-9]{{,4}}{av_censored_id}[ _-]*[0-9]{{2,6}})([^a-z0-9]|$)\n"
+    write_file("av_regex.txt", avRegex, checkDiff=True)
 
     print("Regex:")
-    print(avReg)
-    print("Length:", len(avReg))
+    print(avRegex)
+    print("Length:", len(avRegex))
 
 
 if __name__ == "__main__":
