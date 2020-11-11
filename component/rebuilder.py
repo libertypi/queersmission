132a1b831948a9ce2c3c60caa0912227dfdef733
libertypi <libertypi@gmail.com>
2020-11-11 22:36:14 +0800

    .

--- component/rebuilder.py
+++ component/rebuilder.py
@@ -1,30 +1,134 @@
 #!/usr/bin/env python3
 
 import argparse
+import pickle
 import re
+import subprocess
 import sys
-from collections import Counter
-from concurrent.futures import ThreadPoolExecutor
+from collections import Counter, defaultdict
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from itertools import chain
 from operator import itemgetter
 from pathlib import Path
+from urllib.parse import urljoin
 
 import requests
 from lxml import etree, html
+from torrentool.api import Torrent
+from torrentool.exceptions import TorrentoolException
 
-scriptDir = Path(__file__).parent
-sys.path.insert(0, str(scriptDir.joinpath("../../regenerator").resolve()))
+SCRIPT_DIR = Path(__file__).parent
+REPORT_DIR = Path("/mnt/d/Downloads/jav")
 
+sys.path.insert(0, str(SCRIPT_DIR.joinpath("../../regenerator").resolve()))
 from regenerator import Regen
-
-from mteam_analyzer import MteamScraper
 
 
 class LastPageReached(Exception):
     pass
 
 
-class MtCJKScraper(MteamScraper):
+class MteamScraper:
+
+    DOMAIN = "https://pt.m-team.cc/"
+
+    def __init__(self, dir: Path = REPORT_DIR) -> None:
+
+        self.base_dir = dir if isinstance(dir, Path) else Path(dir)
+        self.id_searcher = re.compile(r"\bid=(?P<id>[0-9]+)").search
+        self.transmission_split = re.compile(r"^\s+(.+?) \([^)]+\)$", flags=re.MULTILINE).findall
+        self.link_finder = etree.XPath(
+            '//*[@id="form_torrent"]/table[@class="torrents"]'
+            '//*[@class="torrenttr"]/table[@class="torrentname"]'
+            '//a[contains(@href, "download.php")]/@href'
+        )
+
+        with open(self.base_dir.joinpath("data"), "rb") as f:
+            self.session: requests.Session = pickle.load(f)[4]
+
+    def fetch(self, page: str, subdir: str, lo: int, hi: int):
+
+        page = self.DOMAIN + page
+        subdir = self.base_dir.joinpath(subdir)
+
+        with ThreadPoolExecutor(max_workers=None) as ex:
+
+            links = as_completed(ex.submit(self._get_link, page, i) for i in range(lo, hi + 1))
+            paths = as_completed(ex.submit(self._fetch_torrent, i, subdir) for f in links for i in f.result())
+
+            for future in paths:
+                try:
+                    with future.result().open("r", encoding="utf-8") as f:
+                        yield f
+                except AttributeError:
+                    pass
+
+    def _get_link(self, page: str, n: int):
+
+        print("Fetching page:", n)
+        for _ in range(3):
+            try:
+                r = self.session.get(page, timeout=7, params={"page": n})
+                r.raise_for_status()
+            except requests.RequestException:
+                pass
+            else:
+                return self.link_finder(html.fromstring(r.content))
+        else:
+            print(f"Downloading page {n} failed.")
+            return ()
+
+    def _fetch_torrent(self, link: str, subdir: Path):
+
+        file = subdir.joinpath(self.id_searcher(link)["id"] + ".txt")
+
+        if not file.exists():
+            print("Fetching torrent:", link)
+
+            for _ in range(3):
+                try:
+                    content = self.session.get(urljoin(self.DOMAIN, link), timeout=7).content
+                except (requests.RequestException, AttributeError):
+                    pass
+                else:
+                    break
+            else:
+                print(f"Downloading torrent failed: {link}")
+                return
+
+            try:
+                filelist = Torrent.from_string(content).files
+                with open(file, "w", encoding="utf-8") as f:
+                    f.writelines(i[0].lower() + "\n" for i in filelist)
+
+            except (TorrentoolException, OSError, TypeError):
+
+                torrent_file = file.with_suffix(".torrent")
+
+                try:
+                    with open(torrent_file, "wb") as f:
+                        f.write(content)
+
+                    filelist = subprocess.check_output(("transmission-show", torrent_file), encoding="utf-8")
+                    filelist = self.transmission_split(filelist, filelist.index("\n\nFILES\n\n"))
+
+                    if not filelist:
+                        raise ValueError
+
+                    with open(file, "w", encoding="utf-8") as f:
+                        f.writelines(s.lower() + "\n" for s in filelist)
+
+                except (subprocess.CalledProcessError, ValueError, OSError):
+                    print(f'Parsing torrent error: "{link}"')
+                    return
+
+                finally:
+                    torrent_file.unlink(missing_ok=True)
+
+        return file
+
+
+class CJKMteamScraper(MteamScraper):
     def __init__(self, *args, **kwargs) -> None:
 
         super().__init__(*args, **kwargs)
@@ -33,7 +137,7 @@ class MtCJKScraper(MteamScraper):
             '//*[@id="form_torrent"]/table[@class="torrents"]//*[@class="torrenttr"]/table[@class="torrentname"]'
         )
         self.title_finder = etree.XPath('(.//a[contains(@href, "details.php")]/@title)[1]')
-        self.link_finder = etree.XPath('.//a[contains(@href, "download.php")]/@href')
+        self.link_finder = etree.XPath('(.//a[contains(@href, "download.php")]/@href)[1]')
 
     def _get_link(self, page: str, n: int):
 
@@ -56,7 +160,7 @@ class MtCJKScraper(MteamScraper):
             try:
                 if self._contains_cjk(title_finder(table)[0]):
                     result.append(link_finder(table)[0])
-            except (TypeError, IndexError):
+            except IndexError:
                 pass
         return result
 
@@ -78,22 +182,25 @@ class MtCJKScraper(MteamScraper):
         )
 
 
-class JavReBuilder:
-    def __init__(self, basedir: Path, remove_rare: bool = False, deep_fetch: bool = False) -> None:
+class JavREBuilder:
+    def __init__(
+        self, report_dir: Path, remove_rare: bool = False, deep_fetch: bool = False, rare_thresh: int = 5
+    ) -> None:
 
-        self.kw_file = basedir.joinpath("keyword.txt")
-        self.prefix_file = basedir.joinpath("id_prefix.txt")
-        self.whitelist_file = basedir.joinpath("id_whitelist.txt")
-        self.rare_file = basedir.joinpath("id_rare.txt")
-        self.output_file = basedir.joinpath("regex.txt")
+        self.kw_file = report_dir.joinpath("keyword.txt")
+        self.prefix_file = report_dir.joinpath("id_prefix.txt")
+        self.whitelist_file = report_dir.joinpath("id_whitelist.txt")
+        self.rare_file = report_dir.joinpath("id_rare.txt")
+        self.output_file = report_dir.joinpath("regex.txt")
 
         self.remove_rare = remove_rare
         if deep_fetch:
             self.limit = {"mteam": 500, "javbus": float("inf"), "javdb": 80}
         else:
             self.limit = {"mteam": 20, "javbus": 50, "javdb": 20}
+        self.rare_thresh = rare_thresh
 
-        self.mtscraper = MtCJKScraper()
+        self.mtscraper = CJKMteamScraper()
         self.session = self.mtscraper.session
         self.session.cookies.set_cookie(
             requests.cookies.create_cookie(domain="www.javbus.com", name="existmag", value="all")
@@ -122,19 +229,17 @@ class JavReBuilder:
 
         return self._get_regex(new_list, name, omitParen)
 
-    @staticmethod
-    def _keyword_strategy(old_list: list) -> list:
-
-        return sorted(set(map(str.lower, filter(None, old_list))))
+    def _keyword_strategy(self, old_list: list) -> list:
+        return sorted(self._normalize_file(old_list))
 
     def _prefix_strategy(self, old_list: list) -> list:
 
-        new_set = set(Regen(tuple(map(str.lower, filter(None, old_list)))).to_text())
+        new_set = set(Regen(self._normalize_file(old_list)).to_text())
         new_set.difference_update(self.fromweb)
 
         try:
             with self.whitelist_file.open("r", encoding="utf-8") as f:
-                whitelist = set(filter(None, map(str.strip, f)))
+                whitelist = self._normalize_file(f)
             new_set.difference_update(whitelist)
             self.fromweb.difference_update(whitelist)
 
@@ -142,7 +247,9 @@ class JavReBuilder:
         except FileNotFoundError:
             pass
 
-        rare = [(i, k) for k in new_set if (i := self.prefix_counter.get(k, 0)) <= 3]
+        thresh = self.rare_thresh
+        get_count = self.prefix_counter.get
+        rare = [(i, k) for k in new_set if (i := get_count(k, 0)) < thresh]
         rare.sort(reverse=True)
 
         if rare and self.remove_rare:
@@ -155,7 +262,11 @@ class JavReBuilder:
         return new_list
 
     @staticmethod
-    def _get_regex(wordlist, name: str, omitOuterParen: bool) -> str:
+    def _normalize_file(wordlist):
+        return set(map(str.lower, filter(None, map(str.strip, wordlist))))
+
+    @staticmethod
+    def _get_regex(wordlist: list, name: str, omitOuterParen: bool) -> str:
 
         regen = Regen(wordlist)
         computed = regen.to_regex(omitOuterParen=omitOuterParen)
@@ -178,8 +289,9 @@ class JavReBuilder:
         result = set(self._scrape_mteam())
         result.update(self._normalize_id(chain(self._scrape_javbus(), self._scrape_javdb(), self._scrape_github())))
 
+        thresh = self.rare_thresh
         self.prefix_counter = Counter(map(itemgetter(0), result))
-        final = {k for k, v in self.prefix_counter.items() if v >= 5 and not kw_filter(k)}
+        final = {k for k, v in self.prefix_counter.items() if v >= thresh and not kw_filter(k)}
 
         print(f"Uniq ID: {len(result)}. Unid prefix: {len(self.prefix_counter)}. Final: {len(final)}.")
         return final
@@ -187,7 +299,7 @@ class JavReBuilder:
     @staticmethod
     def _normalize_id(wordlist):
 
-        matcher = re.compile(r"\s*([a-z]{3,6})[ _-]?0*([0-9]{2,4})(?:[^0-9]|$)").match
+        matcher = re.compile(r"\s*([a-z]{3,7})[ _-]?0*([0-9]{2,5})(?:[^0-9]|$)").match
         for m in filter(None, map(matcher, map(str.lower, wordlist))):
             yield m.group(1, 2)
 
@@ -243,7 +355,7 @@ class JavReBuilder:
                 except LastPageReached:
                     ex.shutdown(wait=False)
 
-    def _scrap_jav(self, arg):
+    def _scrap_jav(self, arg) -> list:
 
         url, xpath = arg
 
@@ -289,38 +401,208 @@ class JavReBuilder:
             with file.open(mode="w", encoding="utf-8") as f:
                 f.write(string)
 
-        print(f"{file} updated.")
+        print(f"{file.name} updated.")
+
+
+def analyze_av(lo: int, hi: int, av_matcher: re.Pattern.search):
+
+    page = "adult.php"
+    unmatched_file = REPORT_DIR / "unmatched.txt"
+    freq_file = REPORT_DIR / "freq.txt"
+    total = unmatched = 0
+    sep = "-" * 80 + "\n"
+
+    prefix_searcher = re.compile(r"\b[0-9]*([a-z]{2,8})[ _-]?[0-9]{2,6}(?:hhb[0-9]?)?\b").search
+    word_finder = re.compile(r"\w{3,}").findall
+
+    flat_counter = defaultdict(set)
+    prefix_counter = Counter()
+    word_counter = Counter()
+    videos = []
+    prefix_tmp = set()
+    word_tmp = set()
+
+    with unmatched_file.open("w", encoding="utf-8") as f:
+
+        for t in MteamScraper().fetch(page, "av", lo, hi):
+            total += 1
+            if any(map(av_matcher, t)):
+                continue
+
+            t.seek(0)
+            videos.extend(filter(is_video, t))
+            if not videos:
+                continue
+
+            unmatched += 1
+            f.write(sep)
+            f.writelines(videos)
+            f.write("\n")
+
+            for m in filter(None, map(prefix_searcher, videos)):
+                prefix = m.group(1)
+                flat_counter[prefix].add(m.group())
+                prefix_tmp.add(prefix)
+            prefix_counter.update(prefix_tmp)
+
+            word_tmp.update(chain.from_iterable(map(word_finder, videos)))
+            word_counter.update(word_tmp)
+
+            videos.clear()
+            prefix_tmp.clear()
+            word_tmp.clear()
+
+        f.write(f"{sep}Total: {total}. Unmatched: {unmatched}.\n")
+
+    prefixes = [(i, len(v), k, v) for k, v in flat_counter.items() if (i := prefix_counter[k]) >= 3]
+    words = [(v, k) for k, v in word_counter.items() if v >= 3]
+    prefixes.sort(reverse=True)
+    words.sort(reverse=True)
+
+    with open(freq_file, "w", encoding="utf-8") as f:
+        f.write("Potential ID Prefixes:\n\n")
+        f.write("{:>6}  {:>6}  {:15}  {}\n{:->80}\n".format("uniq", "occur", "word", "strings", ""))
+        f.writelines(f"{i:6d}  {j:6d}  {k:15}  {s}\n" for i, j, k, s in prefixes)
+
+        f.write("\n\nPotential Keywords:\n\n")
+        f.write("{:>6}  {}\n{:->80}\n".format("uniq", "word", ""))
+        f.writelines(f"{i:6d}  {j}\n" for i, j in words)
+
+    print(f"Done. Results saved to: \n{unmatched_file}\n{freq_file}")
+
+
+def is_video(string: str):
+    return string.rstrip().endswith((".mp4", ".wmv", ".avi", ".iso", ".m2ts"))
+
+
+def analyze_non_av(lo: int, hi: int, av_matcher: re.Pattern.search):
+
+    page = "torrents.php"
+    mismatched_file = REPORT_DIR / "mismatched.txt"
+    word_searcher = re.compile(r"[a-z]+").search
+
+    flat_counter = defaultdict(list)
+    torrent_counter = Counter()
+    tmp = set()
+
+    for f in MteamScraper().fetch(page, "non_av", lo, hi):
+
+        for m in filter(None, map(av_matcher, f)):
+            m = m.group()
+            try:
+                word = word_searcher(m).group()
+            except AttributeError:
+                pass
+            else:
+                flat_counter[word].append(m)
+                tmp.add(word)
+
+        torrent_counter.update(tmp)
+        tmp.clear()
+
+    result = [(torrent_counter[k], len(v), k, set(v)) for k, v in flat_counter.items()]
+    result.sort(reverse=True)
+
+    with mismatched_file.open("w", encoding="utf-8") as f:
+        f.write("{:>6}  {:>6}  {:15}  {}\n{:->80}\n".format("uniq", "occur", "word", "strings", ""))
+        f.writelines(f"{i:6d}  {j:6d}  {k:15}  {s}\n" for i, j, k, s in result)
+
+    print(f"Done. Result saved to: {mismatched_file}")
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=(
+            "build and test regex.\n"
+            "\nbuild:\n"
+            "   %(prog)s\n"
+            "   %(prog)s -r -d\n"
+            "\ntest:\n"
+            "   %(prog)s -m match\n"
+            "   %(prog)s -m match 100\n"
+            "   %(prog)s -m miss 500 600\n"
+        ),
+    )
+
+    parser.add_argument(
+        "-m",
+        "--mode",
+        dest="mode",
+        action="store",
+        default="build",
+        choices=("build", "match", "miss"),
+        help=(
+            "build: build and save regex to file (default)\n"
+            "match: test regex with av torrents\n"
+            "miss: test regex with non-av torrents"
+        ),
+    )
     parser.add_argument(
         "-r",
         "--remove-rare",
         dest="remove_rare",
         action="store_true",
-        help="remove rare id prefix (less than 5 occurrence)",
+        help="when building regex, remove rare id prefixes",
     )
     parser.add_argument(
         "-d",
         "--deep-fetch",
         dest="deep_fetch",
         action="store_true",
-        help="fetch more torrents from web (slower)",
+        help="when building regex, fetch more torrents from web",
     )
-    return parser.parse_args()
+    parser.add_argument(
+        "range",
+        nargs="*",
+        action="store",
+        type=int,
+        help="range of mteam pages for testing, 1 or 2 integers",
+        default=(0, 20),
+    )
+
+    args = parser.parse_args()
+
+    if args.mode != "build":
+        if len(args.range) == 1:
+            args.range.insert(0, 0)
+        elif len(args.range) != 2 or args.range[0] >= args.range[1]:
+            parser.error("Ranges should be 1 or 2 integers (low to high)")
+
+    return args
 
 
 def main():
 
     args = parse_arguments()
 
-    builder = JavReBuilder(scriptDir, args.remove_rare, args.deep_fetch)
-    builder.run()
+    if args.mode == "build":
 
-    print("Regex:")
-    print(builder.regex)
-    print("Length:", len(builder.regex))
+        builder = JavREBuilder(SCRIPT_DIR, args.remove_rare, args.deep_fetch)
+        builder.run()
+
+        print("Regex:")
+        print(builder.regex)
+        print("Length:", len(builder.regex))
+
+    else:
+
+        for dir in "av", "non_av":
+            dir = REPORT_DIR.joinpath(dir)
+            try:
+                if not dir.exists():
+                    dir.mkdir(parents=True)
+            except OSError as e:
+                print(f'Creating "{dir}" failed: {e}')
+                sys.exit()
+
+        with open(SCRIPT_DIR.joinpath("regex.txt"), "r", encoding="utf-8") as f:
+            av_matcher = re.compile(f.read().strip(), flags=re.MULTILINE).search
+
+        if args.mode == "match":
+            analyze_av(*args.range, av_matcher)
+        else:
+            analyze_non_av(*args.range, av_matcher)
 
 
 if __name__ == "__main__":
