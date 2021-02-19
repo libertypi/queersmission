d1db7f9113a395e33f9bb94f95aa83b7a23f388a
libertypi <libertypi@gmail.com>
2021-02-20 07:20:24 +0800

    .

--- component/test.bash
+++ component/test.bash
@@ -2,69 +2,105 @@
 
 export LC_ALL=C LANG=C
 
-cd "${BASH_SOURCE[0]%/*}/.."
-categorize="component/categorize.awk"
-regex_file="component/regex.txt"
+test_regex() {
+  printf '%s\n' "Testing '${regex_file}' on '${driver_dir}'..." "Unmatched items:"
+  grep -Eivf "${regex_file}" <(find "${driver_dir}" -type f -not -path '*/[.@#]*' -regextype 'posix-extended' -iregex '.*\.((bd|w)mv|3gp|asf|avi|flv|iso|m(2?ts|4p|[24kop]v|p([24]|e?g)|xf)|rm(vb)?|ts|vob|webm)' -printf '%P\n')
 
-case $1 in
-  1)
-    TR_TORRENT_DIR='/volume1/video/TV Series'
-    ;;
-  2)
-    TR_TORRENT_DIR='/volume1/video/Films'
-    ;;
-  r)
-    printf '%s\n' "Testing Regex against driver directory..."
-    grep -Eivf "${regex_file}" <(find '/volume1/driver' -type f -not -path '*/[.@#]*' -regextype 'posix-extended' -iregex '.*\.((bd|w)mv|3gp|asf|avi|flv|iso|m(2?ts|4p|[24kop]v|p([24]|e?g)|xf)|rm(vb)?|ts|vob|webm)' -printf '%P\n')
-    printf '%s\n' "Done. These are the unmatched items in driver directory."
+  printf '%s' "Testing '${regex_file}' on '${video_dir}'..."
+  local result="$(grep -Eif "${regex_file}" <(find "${video_dir}" -type f -not -path '*/[.@#]*' -printf '%P\n'))"
+  if [[ -n "${result}" ]]; then
+    printf '%s\n' "Failed. Match:" "${result}"
+  else
+    printf '%s\n' "Passed."
+  fi
+}
 
-    printf '%s\n' "Testing Regex against video directory..."
-    grep -Eif "${regex_file}" <(find '/volume1/video' -type f -not -path '*/[.@#]*' -printf '%P\n')
-    printf '%s\n' "Done. This should show no match."
-    exit
-    ;;
-  *)
-    if [[ -e $1 ]]; then
-      TR_TORRENT_DIR="${1%/*}"
-      files=("${1##*/}")
-    else
-      TR_TORRENT_DIR='/volume2/@transmission'
-    fi
-    ;;
-esac
-if ((${#files[@]} == 0)); then
-  pushd "${TR_TORRENT_DIR}" >/dev/null
-  files=([^@\#.]*)
-  popd >/dev/null
+print_help() {
+  cat <<EOF 1>&2
+usage: ${BASH_SOURCE[0]} [OPTION]... [DIR]
+
+Test ${categorize}.
+
+optional arguments:
+  -h            display this help text and exit
+  -t            test '${tv_dir}'
+  -f            test '${film_dir}'
+  -r            test '${regex_file}' on '${driver_dir}'
+  DIR           test DIR (default: '${seed_dir}')
+EOF
+}
+
+cd "${BASH_SOURCE[0]%/*}" || exit 1
+categorize="categorize.awk"
+regex_file="regex.txt"
+
+video_dir='/volume1/video'
+tv_dir="${video_dir}/TV Series"
+film_dir="${video_dir}/Films"
+driver_dir='/volume1/driver'
+av_dir="${driver_dir}/Temp"
+seed_dir='/volume2/@transmission'
+unset 'TR_TORRENT_DIR' 'names'
+
+while getopts 'tfrh' a; do
+  case "$a" in
+    t) TR_TORRENT_DIR="${tv_dir}" ;;
+    f) TR_TORRENT_DIR="${film_dir}" ;;
+    r)
+      test_regex
+      exit 0
+      ;;
+    h)
+      print_help
+      exit 1
+      ;;
+    *) exit 1 ;;
+  esac
+done
+
+if [[ -z "${TR_TORRENT_DIR+x}" ]]; then
+  shift "$((OPTIND - 1))"
+  if (($#)); then
+    TR_TORRENT_DIR="${1%/*}"
+    names=("${1##*/}")
+  else
+    TR_TORRENT_DIR="${seed_dir}"
+  fi
+fi
+if [[ -z "${names+x}" ]]; then
+  pushd "${TR_TORRENT_DIR}" >/dev/null && names=([^@\#.]*) || exit 1
+  popd >'/dev/null'
 fi
 
-# mkdir "component/profile"
+printf '%s\n\n' "Testing: ${TR_TORRENT_DIR}"
 
-for TR_TORRENT_NAME in "${files[@]}"; do
+for TR_TORRENT_NAME in "${names[@]}"; do
 
   printf '%s\n' "${TR_TORRENT_NAME}"
 
-  for i in dest dest_display; do
+  for i in dest root; do
     IFS= read -r -d '' "$i"
   done < <(
     awk -v REGEX_FILE="${regex_file}" -v TR_TORRENT_DIR="${TR_TORRENT_DIR}" -v TR_TORRENT_NAME="${TR_TORRENT_NAME}" -f "${categorize}"
   )
 
-  if [[ ${TR_TORRENT_DIR} != '/volume2/@transmission' && ${dest_display} != "${TR_TORRENT_DIR}" ]]; then
-    error+=("${TR_TORRENT_NAME}: ${dest_display}")
-    format='\033[31mDest: %s\nDisp: %s\033[0m\n\n'
+  if [[ ${TR_TORRENT_DIR} != "${seed_dir}" && ${root} != "${TR_TORRENT_DIR}" ]]; then
+    error+=("${TR_TORRENT_NAME}: ${root}")
+    format='\033[31m%s\n%s\033[0m\n\n'
   else
-    format='Dest: %s\nDisp: %s\n\n'
+    case "${root}" in
+      "${tv_dir}") format='\033[32m%s\n%s\033[0m\n\n' ;;
+      "${film_dir}") format='\033[33m%s\n%s\033[0m\n\n' ;;
+      "${av_dir}") format='\033[34m%s\n%s\033[0m\n\n' ;;
+      *) format='%s\n%s\n\n' ;;
+    esac
   fi
-  printf "${format}" "${dest}" "${dest_display}"
+  printf "${format}" "Root: ${root}" "Dest: ${dest}"
 
 done
 
 if ((${#error} > 0)); then
-  printf '%s\n' 'Errors:'
-  for e in "${error[@]}"; do
-    printf '%s\n' "${e}"
-  done
+  printf '%s\n' 'Errors:' "${error[@]}"
 else
   printf '%s\n' 'Passed.'
 fi
