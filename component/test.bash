be5db2613b38ce7abc17a0d89f3818c8cc90b517
libertypi <libertypi@gmail.com>
2021-02-26 08:51:44 +0800

    .

--- component/test.bash
+++ component/test.bash
@@ -12,23 +12,23 @@ av_dir="${driver_dir}/Temp"
 seed_dir='/volume2/@transmission'
 
 test_regex() {
-  printf '%s\n' "Testing '${regex_file}' on '${driver_dir}'..." "Unmatched items:"
+  printf 'Testing "%s" on "%s"...\nUmatched items:\n' "${regex_file}" "${driver_dir}" 1>&2
   grep -Eivf "${regex_file}" <(
     find "${driver_dir}" -type f -not -path '*/[.@#]*' -regextype 'posix-extended' \
       -iregex '.+\.((bd|w)mv|3gp|asf|avi|flv|iso|m(2?ts|4p|[24kop]v|p([24]|e?g)|xf)|rm(vb)?|ts|vob|webm)' \
       -printf '%P\n'
   )
 
-  printf '%s' "Testing '${regex_file}' on '${video_dir}'..."
+  printf '\nTesting "%s" on "%s"...' "${regex_file}" "${video_dir}" 1>&2
   local result="$(
     grep -Eif "${regex_file}" <(
       find "${video_dir}" -type f -not -path '*/[.@#]*' -printf '%P\n'
     )
   )"
   if [[ -n "${result}" ]]; then
-    printf '%s\n' "Failed. Match:" "${result}"
+    printf '%s\n' "Failed. Matched items:" "${result}" 1>&2
   else
-    printf '%s\n' "Passed."
+    printf '%s\n' "Passed." 1>&2
   fi
 }
 
@@ -46,28 +46,38 @@ optional arguments:
   -d DIR        test DIR
   -r            test '${regex_file}' on '${driver_dir}'
 EOF
+  exit 1
 }
 
 cd "${BASH_SOURCE[0]%/*}" || exit 1
 TR_TORRENT_DIR="${seed_dir}"
+check=0
 unset names error
 
 while getopts 'htfd:r' a; do
   case "$a" in
-    t) TR_TORRENT_DIR="${tv_dir}" ;;
-    f) TR_TORRENT_DIR="${film_dir}" ;;
+    t)
+      TR_TORRENT_DIR="${tv_dir}"
+      check=1
+      ;;
+    f)
+      TR_TORRENT_DIR="${film_dir}"
+      check=1
+      ;;
     d)
-      TR_TORRENT_DIR="${OPTARG%/*}"
       names=("${OPTARG##*/}")
+      if [[ ${names[0]} == "${OPTARG}" ]]; then
+        TR_TORRENT_DIR="${PWD}"
+      else
+        TR_TORRENT_DIR="${OPTARG%/*}"
+      fi
+      [[ -e "${OPTARG}" ]] && check=1
       ;;
     r)
       test_regex
       exit 0
       ;;
-    h)
-      print_help
-      exit 1
-      ;;
+    h) print_help ;;
     *) exit 1 ;;
   esac
 done
@@ -77,7 +87,7 @@ done
   popd >/dev/null
 }
 
-printf '%s\n\n' "Testing: ${TR_TORRENT_DIR}"
+printf '%s\n\n' "Testing: ${TR_TORRENT_DIR}" 1>&2
 
 for TR_TORRENT_NAME in "${names[@]}"; do
 
@@ -92,23 +102,23 @@ for TR_TORRENT_NAME in "${names[@]}"; do
       -f "${categorize}"
   )
 
-  if [[ ${TR_TORRENT_DIR} != "${seed_dir}" && ${root} != "${TR_TORRENT_DIR}" ]]; then
+  if [[ ${check} == 1 && ${root} != "${TR_TORRENT_DIR}" ]]; then
     error+=("${TR_TORRENT_NAME} -> ${root}")
-    color='31'
+    color=31
   else
     case "${root}" in
-      "${tv_dir}") color='32' ;;
-      "${film_dir}") color='33' ;;
-      "${av_dir}") color='34' ;;
-      *) color='0' ;;
+      "${tv_dir}") color=32 ;;
+      "${film_dir}") color=33 ;;
+      "${av_dir}") color=34 ;;
+      *) color=0 ;;
     esac
   fi
   printf "\033[${color}m%s\n%s\033[0m\n\n" "Root: ${root}" "Dest: ${dest}"
 
 done
 
-if ((${#error} > 0)); then
-  printf '%s\n' 'Errors:' "${error[@]}"
+if ((${#error})); then
+  printf '%s\n' 'Errors:' "${error[@]}" 1>&2
 else
-  printf '%s\n' 'Passed.'
+  printf '%s\n' 'Passed.' 1>&2
 fi
